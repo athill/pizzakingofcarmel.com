2d252f15156b9ca43867a3bb2e45207864ad896e
Andy Hill <andy@andyhill.us>
2013-02-11 19:35:26 -0500

    application.php: added 'lightbox' module. Need to default all modules to false

--- inc/application.php
+++ inc/application.php
@@ -57,30 +57,6 @@ $settings['incroot'] = $settings['fileroot'] . "/inc";
 
 
 
-////TODO: Doing what? Setting path ???
-// if ($webroot != "") {
-// 	$tmp = str_replace($settings['webroot']."/", "", $self);
-// 	echo $tmp.'<br>';
-// } else {
-// }
-// //if ($_SERVER['REMOTE_ADDR'] == '24.1.115.39') echo 'path?!?' . $tmp.'<br />';	//echo $tmp;
-// $tmp = preg_replace("/index.php$/", "", $tmp);
-// echo $tmp.'<br>';
-// $settings['path'] = $tmp;
-// if (strpos($settings['path'], "/") !== 0) {
-// 	$settings['path'] = "/".$settings['path'];
-// }
-
-
-// ////TODO: script ??? doesn't appear to be used.
-// /*$settings["script"] = explode("/", $tmp);
-// if (count($settings["script"]) > 0 && $settings["script"][0] == "") {
-// 	array_shift($settings["script"]);
-// }
-
-
-
-
 // TODO: autoload objects!!!!!!!!
 // http://www.php.net/manual/en/function.spl-autoload-register.php
 
@@ -102,30 +78,21 @@ include_once($settings['incroot'] . "/Logger.class.php");
 // $settings['mailer'] = new Mailer();
 
 ////Menu
-//echo "here0".$incroot."/Menu.class.php";
-
-
 include_once($settings['incroot']."/Menu.class.php");
-
 $xmlfile = $settings['fileroot'].$settings['menufile'];
 if (stripos($self, '/admin/') > -1) {
 	$xmlfile = $settings['fileroot'].'/admin/menu.xml';
 }
-
 $menu = new Menu($xmlfile);
-
 $retval = $menu->buildPathAndSetTitle();
-
-
-//echo 'um';
-//print_r($retval);
 $settings['breadcrumbs'] = $retval['breadcrumbs'];
 $settings['pageTitle'] = $retval['pagetitle'];
+
 //// js/css packages
 $settings['jsModules'] = array();
 $modules = array(
 	"tooltip", "treeTable", "highcharts", "popup", "galleria", "treemenu",
-	"jquery-ui", "textfill"
+	"jquery-ui", "textfill", 'lightbox'
 );
 foreach ($modules as $module) {
 	if (!array_key_exists($module, $settings['jsModules'])) $settings['jsModules'][$module] = false;
