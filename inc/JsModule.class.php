98a87f95264b97fa9ee1ec8e08e4107862fd65f9
Andy Hill <andy@andyhill.us>
2013-02-11 19:34:18 -0500

    jsModule.class: Cleaned up a bit, added 'lightbox' module

--- inc/JsModule.class.php
+++ inc/JsModule.class.php
@@ -5,22 +5,22 @@ class JsModule {
 	function __construct() {
 		$this->modules = array(
 					////Tooltip
-			"tooltip" => array( 
-				"scripts" => array("/js/jquery-tooltip/lib/jquery.bgiframe.js",
-								"/js/jquery-tooltip/lib/jquery.dimensions.js",
-								"/js/jquery-tooltip/jquery.tooltip.js"),
-				"styles" => array("/js/jquery-tooltip/jquery.tooltip.css")
-			),
-			////Tree Table
-			"treeTable" => array( 
-				"scripts" => array("/js/treeTable/src/javascripts/jquery.treeTable.min.js"),
-				"styles" => array("/js/treeTable/src/stylesheets/jquery.treeTable.css")
-			),
-			////High Charts
-			"highcharts" => array(
-				"scripts" => array("/js/Highcharts-2.1.6/js/highcharts.js"),
-				"styles" => array("")
-			),
+			// "tooltip" => array( 
+			// 	"scripts" => array("/js/jquery/jquery.bgiframe.js",
+			// 					"/js/jquery/jquery.dimensions.js",
+			// 					"/js/jquery-tooltip/jquery.tooltip.js"),
+			// 	"styles" => array("/js/jquery-tooltip/jquery.tooltip.css")
+			// ),
+			// ////Tree Table
+			// "treeTable" => array( 
+			// 	"scripts" => array("/js/treeTable/src/javascripts/jquery.treeTable.min.js"),
+			// 	"styles" => array("/js/treeTable/src/stylesheets/jquery.treeTable.css")
+			// ),
+			// ////High Charts
+			// "highcharts" => array(
+			// 	"scripts" => array("/js/Highcharts-2.1.6/js/highcharts.js"),
+			// 	"styles" => array("")
+			// ),
 			////superfish
 			"popup" => array(
 				"scripts" => array("/js/superfish/js/superfish.js",
@@ -28,19 +28,19 @@ class JsModule {
 								"/js/superfish/js/jquery.bgiframe.min.js"),
 				"styles" => array("/js/superfish/css/superfish.css", "/js/superfish/css/superfish-vertical.css")
 			),
-			////Galleria
-			"galleria" => array(
-				"scripts" => array("/js/galleria/galleria-1.2.5.min.js",
-								"/js/galleria/themes/classic/galleria.classic.min.js"),
-				"styles" => array()
-			),
-			////Tree Menu
-			"treemenu" => array(
-				"scripts" => array("/js/jquery.treeview/jquery.treeview.js",
-									"/js/jquery.treeview/lib/jquery.cookie.js"
-				),
-				"styles" => array("/js/jquery.treeview/jquery.treeview.css")
-			),
+			// ////Galleria
+			// "galleria" => array(
+			// 	"scripts" => array("/js/galleria/galleria-1.2.5.min.js",
+			// 					"/js/galleria/themes/classic/galleria.classic.min.js"),
+			// 	"styles" => array()
+			// ),
+			// ////Tree Menu
+			// "treemenu" => array(
+			// 	"scripts" => array("/js/jquery.treeview/jquery.treeview.js",
+			// 						"/js/jquery.treeview/lib/jquery.cookie.js"
+			// 	),
+			// 	"styles" => array("/js/jquery.treeview/jquery.treeview.css")
+			// ),
 			////textfill
 			"textfill" => array( 
 				"scripts" => array("/js/jquery/jquery-textfill.js"),
@@ -50,6 +50,15 @@ class JsModule {
 			"jquery-ui" => array(
 				"scripts" => array("/js/jquery/jquery-ui/js/jquery-ui-1.8.11.custom.min.js"),
 				"styles" => array("/js/jquery/jquery-ui/css/ui-darkness/jquery-ui-1.8.11.custom.css")
+			),
+			////lightbox
+			"lightbox" => array( 
+				"scripts" => array(
+					"/js/jquery/lightbox/jquery.lightbox.js"
+				),
+				"styles" => array(
+					"/js/jquery/lightbox/jquery.lightbox.css"
+				)
 			),
 		);
 	}
