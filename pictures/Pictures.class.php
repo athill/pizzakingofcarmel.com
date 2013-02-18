f69b1c412a3edde52ce097d7cb1292a67acacd8a
Andy Hill <andy@andyhill.us>
2013-02-18 18:23:52 -0500

    pictures: changed terminology from 'comment' to 'title'

--- pictures/Pictures.class.php
+++ pictures/Pictures.class.php
@@ -17,11 +17,11 @@ class Pictures {
 		}
 		foreach ($this->data['sequence'] as $i => $id) {
 			$pic = $this->data['items'][$id];
-			$comment = $pic['comment'];
+			$title = $pic['title'];
 			$file = $id.'.'.$pic['extension'];
 			//// thumbnail
 			$h->startBuffer();
-			$h->img('/img/pictures/thumb/'.$file, $comment);
+			$h->img('/img/pictures/thumb/'.$file, $title);
 			$thumb = trim($h->endBuffer());
 			//// form
 			if ($form) {
@@ -29,8 +29,8 @@ class Pictures {
 				$h->div($thumb, 'class="pics-img"');
 				$h->odiv('class="pics-fields"');
 				$name = 'pic_'.$id;
-				$h->label($name.'_comment', '<strong>Comment:</strong>');
-				$h->intext($name.'_comment', $pic['comment']);
+				$h->label($name.'_title', 'Title:');
+				$h->intext($name.'_title', $pic['title']);
 				$h->br();
 				// http://stackoverflow.com/questions/8806058/delete-jquery-ui-sortable-list-item
 				$h->button($i, 'Delete', 'class="delete"');
@@ -40,15 +40,16 @@ class Pictures {
 			} else {
 				//// render for lightbox
 				$h->odiv('class="pics-thumbs"');
-				$h->a('/img/pictures/'.$file, $thumb, 'rel="lightbox[pictures]" title="'.$pic['comment'].'"');
-				$h->div($pic['comment'], 'class="pics-comment"');
+				$h->a('/img/pictures/'.$file, $thumb, 'rel="lightbox[pictures]" title="'.$pic['title'].'"');
+				$h->div($pic['title'], 'class="pics-title"');
 				$h->cdiv();	
 			}
 		}
 		if ($form) {
 			$h->odiv('class="add"');
 			$h->file('img');
-			$h->intext('content');
+			$h->label('title', 'Title:');
+			$h->intext('title');
 			$h->cdiv('.add');
 			$h->cul('#pics');
 			$h->hidden('sequence', implode(',', $this->data['sequence']));
