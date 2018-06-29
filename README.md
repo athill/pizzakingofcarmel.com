16c1b7c8b7efde6560d115bad551a99399aeccc7
Andy Hill <andy@andyhill.us>
2018-06-29 17:46:04 -0400

    reactified

---
+++ README.md
@@ -0,0 +1,3 @@
+# pizzakingofcarmel.com
+
+Website for my cousin's restaurant, [Pizza King of Carmel](http://pizzakingofcarmel.com)
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,39 +14,13 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+<!--     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css"> -->
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <header id="header">
-           <div class=""> 
-                Pizza King me!
-           </div>
-           <div class=""> 
-                &nbsp;
-           </div>
-           <div class=""> 
-                Pizza King of Carmel
-           </div>
-           <div class=""> 
-                We deliver!
-           </div>
-           <div class=""> 
-                &nbsp;
-           </div>
-           <div class=""> 
-                Pizza King Me!
-           </div>
-        </header>
-        <aside id="left-sidebar">sidebar</aside>
-        <main id="main" class="py-4">
-            @yield('content')
-        </main>
-        <footer id="footer"> © Pizza King of Carmel, {{ date('Y') }} | About Us | Contact Us</footer>
-    </div>
+  @yield('content')
 </body>
 </html>
